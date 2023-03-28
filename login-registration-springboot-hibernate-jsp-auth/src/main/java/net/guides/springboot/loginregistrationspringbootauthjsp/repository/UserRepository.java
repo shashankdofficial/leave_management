7ee10ce0de20a8816c264235215afea1a4827d33
Shashank Dubey <allyouget14@gmail.com>
2023-03-28 18:22:00 +0530

    Updated

--- login-registration-springboot-hibernate-jsp-auth/src/main/java/net/guides/springboot/loginregistrationspringbootauthjsp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.guides.springboot.loginregistrationspringbootauthjsp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.guides.springboot.loginregistrationspringbootauthjsp.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
