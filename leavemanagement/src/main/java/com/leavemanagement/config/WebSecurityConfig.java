fd7d78576bedf5508375a90c7944a426f073c7e6
Abhijeet61 <abhijeetrajput8272@gmail.com>
2023-03-28 05:42:44 -0700

    java

--- leavemanagement/src/main/java/com/leavemanagement/config/WebSecurityConfig.java
+++ leavemanagement/src/main/java/com/leavemanagement/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
