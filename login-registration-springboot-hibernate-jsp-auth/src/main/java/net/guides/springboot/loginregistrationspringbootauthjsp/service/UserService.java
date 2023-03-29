4d6f1e3e655624f15a79c7ca4e6565faf6979c48
Abhijeet61 <abhijeetrajput8272@gmail.com>
2023-03-28 21:21:01 -0700

    remove

--- login-registration-springboot-hibernate-jsp-auth/src/main/java/net/guides/springboot/loginregistrationspringbootauthjsp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package net.guides.springboot.loginregistrationspringbootauthjsp.service;
-
-import net.guides.springboot.loginregistrationspringbootauthjsp.model.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
