a91b4889eba5558bb451a82f92d9816586be3a03
Shashank Dubey <allyouget14@gmail.com>
2023-03-30 19:13:03 +0530

    Changed basic things

--- top.inc.php
+++ top.inc.php
@@ -28,14 +28,14 @@ if(!isset($_SESSION['ROLE'])){
          <nav class="navbar navbar-expand-sm navbar-default">
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                <ul class="nav navbar-nav">
-                  <li class="menu-title">Menu</li>
+                  <li class="menu-title">Welcome <?php echo $_SESSION['USER_NAME']?></li>
                   <?php if($_SESSION['ROLE']==1){ ?>
-				  <li class="menu-item-has-children dropdown">
+				      <!-- <li class="menu-item-has-children dropdown">
                      <a href="index.php" > Department Master</a>
                   </li>
                   <li class="menu-item-has-children dropdown">
                      <a href="leave_type.php" > Leave Type Master</a>
-                  </li>
+                  </li> -->
 				  <!-- <li class="menu-item-has-children dropdown">
                      <a href="employee.php" > Employee Master</a>
                   </li> -->
@@ -60,19 +60,19 @@ if(!isset($_SESSION['ROLE'])){
          <header id="header" class="header">
             <div class="top-left">
                <div class="navbar-header">
-                  <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Logo"></a>
-                  <a class="navbar-brand hidden" href="index.php"><img src="" alt="Logo"></a>
+                  <a class="navbar-brand" href="leave.php"><img src="images/logo.png" alt="Logo"></a>
+                  <!-- <a class="navbar-brand hidden" href="index.php"><img src="" alt="Logo"></a> -->
                   <!-- <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a> -->
                </div>
             </div>
             <div class="top-right">
                <div class="header-menu">
-                  <div class="user-area dropdown float-right">
-                     <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome <?php echo $_SESSION['USER_NAME']?> Log-out</a>
-                     <div class="user-menu dropdown-menu">
-                        <a class="nav-link" href="logout.php"><i class="fa fa-power-off"></i>Logout</a>
-                     </div>
-                  </div>
+                  <!-- <div class="user-area dropdown float-right">
+                     <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Log-out</a>
+                     <div class="user-menu dropdown-menu"> -->
+                        <a class="nav-link" href="logout.php"><i class="fa fa-power-off"></i> Logout</a>
+                     <!-- </div>
+                  </div> -->
                </div>
             </div>
          </header>
